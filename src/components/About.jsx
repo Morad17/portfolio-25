908028ffdb7b83a83410ed2179ce2968f8e2a658
Morad17 <morad123@hotmail.co.uk>
2025-07-21 12:08:51 +0100

    organising layout

---
+++ src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="about page">
+      <div className="background-section">
+        <p className="background-text">
+          Hi I'm Morad, a frontend developer from the UK. Aesthetics,
+          Application and Avant-garde are factors I consider most when designing
+          projects.My tools of choice are primarilty React, Scss & Node, with
+          others implemented based on the project. If you have a project in
+          mind, or a killer idea send me a message!
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,15 +2,15 @@ import React from "react";
 
 const Hero = () => {
   return (
-    <div className="hero">
+    <div className="hero page">
       <div className="title-row">
         <h2 className="title">Hi I'm Morad</h2>
-        <h3 className="sub-title">a Frontend Developer</h3>
+        <h3 className="heading">a Frontend Developer</h3>
       </div>
       <div className="about-me-row">
         <p className="about-me-text">
           Where creativity meets code; I craft thoughtful digital solutions with
-          aesthetics and funcitonality at the core. From concept to code, I turn
+          aesthetics and funcitonality at the core.From concept to code, I turn
           bold ideas into reality.
         </p>
       </div>
