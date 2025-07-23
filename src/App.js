4635ceff09157791fe79ffe94b5c2fc91ea1d8bc
Morad17 <morad123@hotmail.co.uk>
2025-07-23 21:58:20 +0100

    updates

--- src/App.js
+++ src/App.js
@@ -2,12 +2,16 @@ import { Outlet, createBrowserRouter, RouterProvider } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import Particles from "./components/ParticleAnimation";
 
 function App() {
   const Layout = () => {
     return (
       <div className="main-layout">
         <Navbar />
+        <div className="background-particles">
+          <Particles particleCount={300} moveParticlesOnHover={true} />
+        </div>
         <div className="content">
           <Outlet />
           <Footer />
