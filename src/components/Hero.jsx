31d59bf307dda3abef7b430fb85fad98803a27c8
Morad17 <morad123@hotmail.co.uk>
2025-07-26 22:31:58 +0100

    added spaceman model

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,8 @@
-import { motion, useScroll, useTransform, useSpring } from "framer-motion";
 import React, { useRef } from "react";
+import { motion, useScroll, useTransform, useSpring } from "framer-motion";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Environment } from "@react-three/drei";
+import { Spaceman } from "./Spaceman";
 
 import sun from "../assets/images/sunset-sun.png";
 
@@ -16,37 +19,50 @@ const Hero = () => {
     restDelta: 0.001,
   });
 
-  const backgroundY = useTransform(smoothProgress, [0, 1], ["0%", "280%"]);
+  const backgroundY = useTransfs ee4e orm(smoothProgress, [0, 1], ["0%", "280%"]);
   const backgroundX = useTransform(smoothProgress, [0, 1], ["0%", "-120%"]);
 
   return (
-    <div className="hero page">
-      <div className="sunset-parallax-home" ref={ref}>
-        <motion.div
-          className="sunset-sun"
-          style={{
-            backgroundImage: `url(${sun})`,
-            y: backgroundY,
-            x: backgroundX,
-          }}
-        />
-      </div>
-      <div className="section-content">
-        <div className="title-row">
-          <h2 className="title">Hi I'm Morad</h2>
-          <h3 className="heading">a Frontend Developer</h3>
+    <div className="hero page" ref={ref}>
+      <motion.div
+        className="sunset-sun"
+        style={{
+          backgroundImage: `url(${sun})`,
+          y: backgroundY,
+          x: backgroundX,
+        }}
+      />
+      <section className="center-section">
+        <div className="section-content">
+          <div className="title-row">
+            <h2 className="title">Hi I'm Morad</h2>
+            <h3 className="heading">a Frontend Developer</h3>
+          </div>
+
+          <div className="about-me-row">
+            <p className="about-me-text">
+              Where creativity meets code; I craft thoughtful digital solutions
+              with aesthetics and funcitonality at the core.From concept to
+              code, I turn bold ideas into reality.
+            </p>
+          </div>
+          <div className="links-row">
+            <button className="contact-btn">Get In Touch</button>
+          </div>
         </div>
-        <div className="about-me-row">
-          <p className="about-me-text">
-            Where creativity meets code; I craft thoughtful digital solutions
-            with aesthetics and funcitonality at the core.From concept to code,
-            I turn bold ideas into reality.
-          </p>
+        <div className="model-row">
+          <Canvas
+            camera={{ position: [1, 1, 3] }}
+            style={{ width: "100%", height: "100%" }}
+          >
+            <Environment preset="dawn" />
+            <Spaceman
+              scale={[1, 1, 1]}
+              rotation={[Math.PI / -4, 0, -Math.PI / -7]} // Added Z-axis tilt (-15 degrees)
+            />
+          </Canvas>
         </div>
-        <div className="links-row">
-          <button className="contact-btn">Get In Touch</button>
-        </div>
-      </div>
+      </section>
     </div>
   );
 };
